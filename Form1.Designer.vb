41b8031b5dab4d1dfd63ae471aacd338598f0831
Dell-User <Dell-User@DESKTOP-I2HH80B>
2023-10-04 07:39:35 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnDivision = New Button()
+        btnMultiply = New Button()
+        btnSubtract = New Button()
+        btnAdd = New Button()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        TextBox3 = New TextBox()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' btnDivision
+        ' 
+        btnDivision.Location = New Point(452, 221)
+        btnDivision.Name = "btnDivision"
+        btnDivision.Size = New Size(112, 34)
+        btnDivision.TabIndex = 0
+        btnDivision.Text = "Divide"
+        btnDivision.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.Location = New Point(299, 221)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(112, 34)
+        btnMultiply.TabIndex = 0
+        btnMultiply.Text = "Multiply"
+        btnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.Location = New Point(159, 221)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(112, 34)
+        btnSubtract.TabIndex = 0
+        btnSubtract.Text = "Subtract"
+        btnSubtract.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(24, 221)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(112, 34)
+        btnAdd.TabIndex = 0
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(373, 26)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(181, 32)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(122, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "Enter Number"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(181, 81)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(122, 25)
+        Label2.TabIndex = 2
+        Label2.Text = "Enter Number"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(373, 75)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(392, 121)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 25)
+        Label3.TabIndex = 3
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(373, 118)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(150, 31)
+        TextBox3.TabIndex = 1
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(181, 118)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(70, 25)
+        Label4.TabIndex = 2
+        Label4.Text = "Answer"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label3)
+        Controls.Add(Label4)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(btnAdd)
+        Controls.Add(btnSubtract)
+        Controls.Add(btnMultiply)
+        Controls.Add(btnDivision)
+        Name = "Form1"
+        Text = "Using Controls"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnDivision As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Windows.Forms.Design
+
+Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        ' MsgBox("The sum is  =" & Val(TextBox1.Text) + Val(TextBox2.Text))
+        Label3.Text = Val(TextBox1.Text) + Val(TextBox2.Text)
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        MsgBox("The subtraction is  =" & Val(TextBox1.Text) - Val(TextBox2.Text))
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        MsgBox("The multiplication is  =" & Val(TextBox1.Text) * Val(TextBox2.Text))
+    End Sub
+
+    Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+        MsgBox("The division is  =" & Val(TextBox1.Text) / Val(TextBox2.Text))
+    End Sub
+End Class
